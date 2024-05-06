cc1fc4fd022ff78283f46726ea39c446650eaafd
MatiasRamirez315 <101595722+MatiasRamirez315@users.noreply.github.com>
2024-05-05 21:57:12 -0300

    Se sube portfolio

---
+++ codigoPortfolio.js
@@ -0,0 +1,10 @@
+CodigoPortfolio.js
+
+function Enviar(){
+    var Nombre = document.getElementById("nombre").value;
+    var Mail = document.getElementById("Email").value;
+    var Coment = document.getElementById("comentario").value;
+
+    alert ("hola " + Nombre + " tu comentario fue enviado");
+
+}
